ce96d07ead018a603fcb4680f2695dd46fe99933
Kushal Sigdel <119459843+sigdelkushal@users.noreply.github.com>
2023-10-26 20:26:04 -0400

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -8,14 +7,19 @@ using namespace std;
 template <typename data_type,typename key_type>
 class Node
 {
-    public:
-        Node* left; //is there a way to make these private? friend fct?
-        Node* right;
-        Node* p;
+    private:
         data_type data;
         key_type key;
+
+    public:
+        Node<* left; 
+        Node* right;
+        Node* p;
         
+        Node();
         Node(data_type d, key_type k); //no arg constructor. creates empty Node object.
+        data_type get_data();
+        key_type  get_key();
         
     
 };
@@ -23,19 +27,19 @@ class Node
 template <typename data_type,typename key_type>
 class BST
 {
-    Node<data_type,key_type>* root;
+    private:
+        Node<data_type,key_type>* root;
 
+        void transplant(Node<data_type,key_type>* u,Node<data_type,key_type>* v);
+        Node<data_type,key_type> *treemin(Node<data_type,key_type> *x);
     public:
         BST(); //no arg constructor. creates empty Node object.
     
         bool empty(); //
-        void insert(data_type data, key_type k); //
+        void insert(data_type d, key_type k); //
         data_type get(key_type key); //
-        void transplant(Node<data_type,key_type>* u,Node<data_type,key_type>* v);
-        Node<data_type,key_type>* min(Node<data_type,key_type>* m);//
         void remove(key_type key); //
         data_type max_data(); //
-        data_type max_data_help(Node<data_type,key_type>* x);
         key_type max_key(); //
         data_type min_data(); //
         key_type min_key(); //
